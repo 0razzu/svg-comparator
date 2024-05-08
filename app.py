675ec28a945eea383552181d2ef7443cd3abf892
0razzu <0razzu@protonmail.com>
2024-05-08 12:54:23 +0600

    arrows

--- app.py
+++ app.py
@@ -1,12 +1,40 @@
-import math
 import tkinter as tk
-from collections import namedtuple
 from tkinter import filedialog
+
 import cairosvg
+from svgpathtools import svg2paths, svg2paths2, CubicBezier, QuadraticBezier
 
-from svgpathtools import svg2paths, svg2paths2, CubicBezier, QuadraticBezier, Arc, Line
 
-Point = namedtuple('Point', ['x', 'y'])
+class Point:
+    def __init__(self, x, y, whose=None):
+        self.x = x
+        self.y = y
+        self.whose = whose
+
+    def __add__(self, other):
+        if isinstance(other, Point):
+            return Point(self.x + other.x, self.y + other.y, self.whose)
+        else:
+            return Point(self.x + other, self.y + other, self.whose)
+
+    def __radd__(self, other):
+        return self + other
+
+    def __sub__(self, other):
+        if isinstance(other, Point):
+            return Point(self.x - other.x, self.y - other.y, self.whose)
+        else:
+            return Point(self.x - other, self.y - other, self.whose)
+
+    def __rsub__(self, other):
+        return self - other
+
+    def __mul__(self, coef):
+        return Point(self.x * coef, self.y * coef, self.whose)
+
+    def __rmul__(self, coef):
+        return Point(coef * self.x, coef * self.y, self.whose)
+
 
 COLOR_LINE = 'black'
 COLOR_CUBIC = 'blue'
@@ -14,6 +42,7 @@ COLOR_QUAD = 'green'
 COLOR_ARC = 'magenta'
 COLOR_INT_POINT = 'orange'
 COLOR_END_POINT = 'red'
+COLOR_CONNECTOR = 'violet'
 
 DIR_LEFT = Point(-1, 0)
 DIR_RIGHT = Point(1, 0)
@@ -21,7 +50,7 @@ DIR_UP = Point(0, -1)
 DIR_DOWN = Point(0, 1)
 
 
-def _to_x_y(complex_point):
+def _complex_to_point(complex_point):
     return Point(complex_point.real, complex_point.imag)
 
 
@@ -68,8 +97,8 @@ class SVGComparator:
 
     def bind_events(self):
         self.root.bind('<MouseWheel>', self.mouse_wheel)
-        self.root.bind('<Command-equal>', self.scale_up)
-        self.root.bind('<Command-minus>', self.scale_down)
+        self.root.bind('<Command-equal>', lambda _: self.scale_up)
+        self.root.bind('<Command-minus>', lambda _: self.scale_down)
         self.root.bind("<Left>", lambda _: self.move_canvas(DIR_LEFT))
         self.root.bind("<Right>", lambda _: self.move_canvas(DIR_RIGHT))
         self.root.bind("<Up>", lambda _: self.move_canvas(DIR_UP))
@@ -81,7 +110,7 @@ class SVGComparator:
         self.root.bind("<Shift-Down>", lambda _: self.move_canvas(DIR_DOWN, fast_move_speed))
         self.root.bind("<space>", lambda _: self.toggle_point_visibility())
         self.canvas.bind("<ButtonPress-1>", self.on_canvas_click)
-        self.canvas.bind("<Double-Button-1>", lambda _: self.move_canvas_to_origin)
+        self.canvas.bind("<Double-Button-1>", lambda _: self.move_canvas_to_origin())
         self.canvas.bind("<B1-Motion>", self.on_canvas_drag)
 
     def open_svg(self):
@@ -91,26 +120,31 @@ class SVGComparator:
             self.update_canvas()
 
     def _collect_points(self):
+        self.int_points = []
+        self.end_points = []
         svg, _ = svg2paths(self.filename)
 
         for path in svg:
             for segment in path:
-                start = _to_x_y(segment.start)
-                end = _to_x_y(segment.end)
+                start = _complex_to_point(segment.start)
+                end = _complex_to_point(segment.end)
 
                 if len(self.end_points) == 0 or self.end_points[-1] != start:
                     self.end_points.append(start)
                 self.end_points.append(end)
 
                 if type(segment) is CubicBezier:
-                    control1 = _to_x_y(segment.control1)
-                    control2 = _to_x_y(segment.control2)
+                    control1 = _complex_to_point(segment.control1)
+                    control1.whose = [start]
+                    control2 = _complex_to_point(segment.control2)
+                    control2.whose = [end]
 
                     self.int_points.append(control1)
                     self.int_points.append(control2)
 
                 elif type(segment) is QuadraticBezier:
-                    control = _to_x_y(segment.control)
+                    control = _complex_to_point(segment.control)
+                    control.whose = [start, end]
 
                     self.int_points.append(control)
 
@@ -118,10 +152,10 @@ class SVGComparator:
         self.draw_svg()
         self.draw_points()
 
-    def _scale(self, point):
-        return self.scale * point.x, self.scale * point.y
-
     def draw_svg(self):
+        if not self.filename:
+            return
+
         self.canvas.delete('svg')
 
         _, _, meta = svg2paths2(self.filename)
@@ -131,37 +165,44 @@ class SVGComparator:
                                output_width=self.scale * width,
                                output_height=self.scale * height)
         image = tk.PhotoImage(data=png)
-        self.canvas.create_image(*self.svg_lt_pos, anchor=tk.NW, image=image, tags=('svg',))
+        self.canvas.create_image(self.svg_lt_pos.x, self.svg_lt_pos.y, anchor=tk.NW, image=image, tags=('svg',))
         self.canvas.image = image  # Keeping a reference to the image to prevent garbage collection
 
     def _draw_points(self, points, color):
         if len(points) > 0:
             for point in points:
-                self.canvas.create_oval(self.svg_lt_pos.x + point.x * self.scale - 3,
-                                        self.svg_lt_pos.y + point.y * self.scale - 3,
-                                        self.svg_lt_pos.x + point.x * self.scale + 3,
-                                        self.svg_lt_pos.y + point.y * self.scale + 3,
+                pos = self.svg_lt_pos + point * self.scale
+                self.canvas.create_oval(pos.x - 3, pos.y - 3, pos.x + 3, pos.y + 3,
                                         fill=color, outline=COLOR_LINE, tags=('point',))
 
     def draw_points(self):
-        self.canvas.delete('point')
+        self.canvas.delete('point', 'connector')
 
         if self.points_visible:
-            self._draw_points(self.end_points, COLOR_END_POINT)
             self._draw_points(self.int_points, COLOR_INT_POINT)
 
-    def scale_up(self, event=None):
+            for int_point in self.int_points:
+                for end_point in int_point.whose:
+                    self.canvas.create_line(self.svg_lt_pos.x + self.scale * int_point.x,
+                                            self.svg_lt_pos.y + self.scale * int_point.y,
+                                            self.svg_lt_pos.x + self.scale * end_point.x,
+                                            self.svg_lt_pos.y + self.scale * end_point.y,
+                                            fill=COLOR_CONNECTOR, width=1, arrow=tk.FIRST, tags=('connector',))
+
+            self._draw_points(self.end_points, COLOR_END_POINT)
+
+    def scale_up(self):
         self.scale *= 1.1
         self.update_canvas()
 
-    def scale_down(self, event=None):
+    def scale_down(self):
         self.scale /= 1.1
         self.update_canvas()
 
     def move_canvas(self, direction, speed=1):
-        delta = Point(speed * direction.x, speed * direction.y)
+        delta = speed * direction
 
-        self.svg_lt_pos = Point(self.svg_lt_pos.x + delta.x, self.svg_lt_pos.y + delta.y)
+        self.svg_lt_pos = self.svg_lt_pos + delta
         self.canvas.move('all', delta.x, delta.y)
 
     def mouse_wheel(self, event):
@@ -179,12 +220,14 @@ class SVGComparator:
         self.drag_data = Point(event.x, event.y)
 
     def on_canvas_drag(self, event):
-        self.move_canvas(Point(event.x - self.drag_data.x, event.y - self.drag_data.y))
-        self.drag_data = Point(event.x, event.y)
+        dest = Point(event.x, event.y)
+        self.move_canvas(dest - self.drag_data)
+        self.drag_data = dest
 
     def move_canvas_to_origin(self):
         self.canvas.moveto('all', 0, 0)
         self.drag_data = Point(0, 0)
+        self.svg_lt_pos = Point(0, 0)
 
 
 if __name__ == '__main__':
